fe244f73eb91f2243406e1184e6bc4163be28234
garrett <gn9012@gmail.com>
2017-08-03 20:33:29 +0000

    #3 -- added donation money and count

--- page/models.py
+++ page/models.py
@@ -6,6 +6,8 @@ class Page(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(blank=True)
+    donation_count = models.IntegerField(default=0)
+    donation_money = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
