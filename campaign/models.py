00b29ddf273a53b6560007fda8bba714b934e470
garrett <gn9012@gmail.com>
2017-08-04 18:27:19 +0000

    #3 -- added goal to campaign and displayed on page

--- campaign/models.py
+++ campaign/models.py
@@ -6,6 +6,7 @@ class Campaign(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     campaign_slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(blank=True)
+    goal = models.IntegerField(default=0)
     donation_count = models.IntegerField(default=0)
     donation_money = models.IntegerField(default=0)
     TYPE_CHOICES = (
