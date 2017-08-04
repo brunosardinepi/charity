7c58ef91e6dcf79d73025b9e22a46fd7d3439d4c
garrett <gn9012@gmail.com>
2017-08-04 02:20:44 +0000

    #4 -- added is_active check for campaigns

--- campaign/models.py
+++ campaign/models.py
@@ -21,6 +21,7 @@ class Campaign(models.Model):
         on_delete=models.CASCADE,
         related_name='campaigns',
     )
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
--- page/views.py
+++ page/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import get_object_or_404, render
 
 from . import models
+from campaign import models as CampaignModels
 
 
 def page(request, page_slug):
     page = get_object_or_404(models.Page, page_slug=page_slug)
-    return render(request, 'page/page.html', {'page': page})
+    active_campaigns = CampaignModels.Campaign.objects.filter(is_active=True)
+    inactive_campaigns = CampaignModels.Campaign.objects.filter(is_active=False)
+    return render(request, 'page/page.html', {
+                                            'page': page,
+                                            'active_campaigns': active_campaigns,
+                                            'inactive_campaigns': inactive_campaigns
+                                            })
