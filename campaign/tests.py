2867186c9129467dc75a9da1b4448c0ad2901701
garrett <gn9012@gmail.com>
2017-08-04 03:12:08 +0000

    #4 -- added tests for campaign and page because i forgot to change branches to #3 for page tests

--- campaign/tests.py
+++ campaign/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+import django
 
-# Create your tests here.
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import Client, RequestFactory, TestCase
+
+from . import views
+from . import models
+from page import models as PageModels
+
+
+class CampaignTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
+
+        self.user = User.objects.create_user(
+                                            username='testuser',
+                                            email='test@test.test',
+                                            password='testpassword'
+                                            )
+
+        self.page = PageModels.Page.objects.create(
+                                        name='Test Page',
+                                        )
+
+        self.campaign = models.Campaign.objects.create(
+                                            name='Test Campaign',
+                                            page=self.page,
+                                            description='This is a description for Test Campaign.',
+                                            donation_count='5',
+                                            donation_money='100'
+                                            )
+
+    def test_campaign_exists(self):
+        """
+        Test campaign exists
+        """
+
+        # get queryset that contains all campaigns
+        campaigns = models.Campaign.objects.all()
+
+        # make sure the test campaign exists in the queryset
+        self.assertIn(self.campaign, campaigns)
+
+    def test_campaign_status_logged_out(self):
+        """
+        Campaign page returns HTTP 200
+        """
+
+        # create GET request
+        request = self.factory.get('home')
+
+        # simulate logged-out user
+        request.user = AnonymousUser()
+
+        # test the view
+        response = views.campaign(request, self.page.page_slug, self.campaign.campaign_slug)
+
+        # check that the response is 200 OK
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.campaign.name, status_code=200)
+        self.assertContains(response, self.campaign.description, status_code=200)
+        self.assertContains(response, self.campaign.donation_count, status_code=200)
+        self.assertContains(response, self.campaign.donation_money, status_code=200)
+
+    def test_campaign_status_logged_in(self):
+        """
+        Campaign page returns HTTP 200
+        """
+
+        # create GET request
+        request = self.factory.get('home')
+
+        # simulate logged-out user
+        request.user = self.user
+
+        # test the view
+        response = views.campaign(request, self.page.page_slug, self.campaign.campaign_slug)
+
+        # check that the response is 200 OK
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.campaign.name, status_code=200)
+        self.assertContains(response, self.campaign.description, status_code=200)
+        self.assertContains(response, self.campaign.donation_count, status_code=200)
+        self.assertContains(response, self.campaign.donation_money, status_code=200)
--- page/tests.py
+++ page/tests.py
@@ -1,3 +1,101 @@
-from django.test import TestCase
+import django
 
-# Create your tests here.
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import Client, RequestFactory, TestCase
+
+from . import views
+from . import models
+from campaign import models as CampaignModels
+
+
+class CampaignTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
+
+        self.user = User.objects.create_user(
+                                            username='testuser',
+                                            email='test@test.test',
+                                            password='testpassword'
+                                            )
+
+        self.page = models.Page.objects.create(
+                                                name='Test Page',
+                                                description='This is a description for Test Page.',
+                                                donation_count='5',
+                                                donation_money='100'
+                                                )
+
+        self.campaign = CampaignModels.Campaign.objects.create(
+                                            name='Test Campaign',
+                                            page=self.page,
+                                            description='This is a description for Test Campaign.',
+                                            donation_count='5',
+                                            donation_money='100'
+                                            )
+
+        self.campaign2 = CampaignModels.Campaign.objects.create(
+                                            name='Another One',
+                                            page=self.page,
+                                            description='My cat died yesterday',
+                                            donation_count='7',
+                                            donation_money='60'
+                                            )
+
+    def test_page_exists(self):
+        """
+        Test page exists
+        """
+
+        # get queryset that contains all pages
+        pages = models.Page.objects.all()
+
+        # make sure the test page exists in the queryset
+        self.assertIn(self.page, pages)
+
+    def test_page_status_logged_out(self):
+        """
+        Page returns HTTP 200
+        """
+
+        # create GET request
+        request = self.factory.get('home')
+
+        # simulate logged-out user
+        request.user = AnonymousUser()
+
+        # test the view
+        response = views.page(request, self.page.page_slug)
+
+        # check that the response is 200 OK
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.page.name, status_code=200)
+        self.assertContains(response, self.page.description, status_code=200)
+        self.assertContains(response, self.page.donation_count, status_code=200)
+        self.assertContains(response, self.page.donation_money, status_code=200)
+        self.assertContains(response, self.campaign, status_code=200)
+        self.assertContains(response, self.campaign2, status_code=200)
+
+    def test_page_status_logged_in(self):
+        """
+        Page returns HTTP 200
+        """
+
+        # create GET request
+        request = self.factory.get('home')
+
+        # simulate logged-out user
+        request.user = self.user
+
+        # test the view
+        response = views.page(request, self.page.page_slug)
+
+        # check that the response is 200 OK
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.page.name, status_code=200)
+        self.assertContains(response, self.page.description, status_code=200)
+        self.assertContains(response, self.page.donation_count, status_code=200)
+        self.assertContains(response, self.page.donation_money, status_code=200)
+        self.assertContains(response, self.campaign, status_code=200)
+        self.assertContains(response, self.campaign2, status_code=200)
