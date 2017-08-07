50f4313a430bf602d7b58f2a90e8c6aac08cca41
garrett <gn9012@gmail.com>
2017-08-07 21:16:59 +0000

    #2 -- search and filter work independently of each other

--- search/views.py
+++ search/views.py
@@ -13,6 +13,24 @@ def search(request):
 def results(request):
     if request.is_ajax():
         q = request.GET.get('q')
+        f = request.GET.get('f')
+
+        if q == "0":
+            q = False
+        elif f == "0":
+            f = False
+
+        if all([q, f]):
+            print("q and f")
         if q:
             results = PageModels.Page.objects.filter(name__unaccent__trigram_similar=q).order_by('name')
+            return render(request, 'search/results.html', {'results': results})
+        if f:
+            f = f.split(",")
+            results = []
+            for i in f:
+                query_objects = PageModels.Page.objects.filter(category=i).order_by('name')
+                if query_objects:
+                    for object in query_objects:
+                        results.append(object)
             return render(request, 'search/results.html', {'results': results})
