9c1b7a989f29dfcb5c62cf61cb0387591d4f8656
garrett <gn9012@gmail.com>
2017-08-07 19:15:46 +0000

    #3 -- fixed campaign filter on pages

--- page/views.py
+++ page/views.py
@@ -9,8 +9,8 @@ from campaign import models as CampaignModels
 
 def page(request, page_slug):
     page = get_object_or_404(models.Page, page_slug=page_slug)
-    active_campaigns = CampaignModels.Campaign.objects.filter(is_active=True)
-    inactive_campaigns = CampaignModels.Campaign.objects.filter(is_active=False)
+    active_campaigns = CampaignModels.Campaign.objects.filter(page=page, is_active=True)
+    inactive_campaigns = CampaignModels.Campaign.objects.filter(page=page, is_active=False)
     return render(request, 'page/page.html', {
                                             'page': page,
                                             'active_campaigns': active_campaigns,
