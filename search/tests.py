b228ae811103a0ace0136ada9e59645b85abd78a
garrett <gn9012@gmail.com>
2017-08-07 19:52:58 +0000

    #2 -- added tests

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import Client, RequestFactory, TestCase
 
-# Create your tests here.
+from . import views
+
+
+class SearchTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.user = User.objects.create_user(
+                        username='testuser',
+                        email='test@test.test',
+                        password='testpassword',
+                        )
+
+    def test_search_page_logged_in(self):
+        """Search page is accessible when logged in"""
+        request = self.factory.get('home')
+        request.user = self.user
+        response = views.search(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_page_logged_out(self):
+        """Search page is accessible when logged out"""
+        request = self.factory.get('home')
+        request.user = AnonymousUser()
+        response = views.search(request)
+        self.assertEqual(response.status_code, 200)
