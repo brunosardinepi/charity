9759621b2ec4f8937b48dd9e9504d9b70eb5645e
garrett <gn9012@gmail.com>
2017-08-10 21:31:16 +0000

    #19 -- button changes colors and functions, but won't function multiple times yet

--- page/views.py
+++ page/views.py
@@ -5,16 +5,28 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from . import forms
 from . import models
 from campaign import models as CampaignModels
+from userprofile.models import UserProfile
+
+import json
 
 
 def page(request, page_slug):
     page = get_object_or_404(models.Page, page_slug=page_slug)
     active_campaigns = CampaignModels.Campaign.objects.filter(page=page, is_active=True)
     inactive_campaigns = CampaignModels.Campaign.objects.filter(page=page, is_active=False)
+    try:
+        user_subscription_check = page.subscribers.get(user_id=request.user.pk)
+    except UserProfile.DoesNotExist:
+        user_subscription_check = None
+    if user_subscription_check:
+        subscribe_attr = {"name": "unsubscribe", "value": "Unsubscribe", "color": "red"}
+    else:
+        subscribe_attr = {"name": "subscribe", "value": "Subscribe", "color": "green"}
     return render(request, 'page/page.html', {
                                             'page': page,
                                             'active_campaigns': active_campaigns,
-                                            'inactive_campaigns': inactive_campaigns
+                                            'inactive_campaigns': inactive_campaigns,
+                                            'subscribe_attr': subscribe_attr
                                             })
 
 @login_required
@@ -31,6 +43,18 @@ def page_edit(request, page_slug):
         raise Http404
     return render(request, 'page/page_edit.html', {'page': page, 'form': form})
 
-def subscribe(request):
-    print("subscribe")
-    return HttpResponse("OK")
+def subscribe(request, page_pk, action=None):
+    page = get_object_or_404(models.Page, pk=page_pk)
+    print("here")
+    if action == "subscribe":
+        print("subscribe")
+        page.subscribers.add(request.user.userprofile)
+        new_subscribe_attr = {"name": "unsubscribe", "value": "Unsubscribe", "color": "red"}
+    elif action == "unsubscribe":
+        print("unsubscribe")
+        page.subscribers.remove(request.user.userprofile)
+        new_subscribe_attr = {"name": "subscribe", "value": "Subscribe", "color": "green"}
+    else:
+        print("something went wrong")
+    new_subscribe_attr = json.dumps(new_subscribe_attr)
+    return HttpResponse(new_subscribe_attr)
