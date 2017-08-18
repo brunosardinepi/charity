52013bb7c9f7300a4372a197956aeeae38529f3c
Mike Mosley <mike@mike.page.fund>
2017-08-18 15:41:59 +0000

    #8 -- avatar displays in title but is not going through validation checks

--- pagefund/urls.py
+++ pagefund/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 from page import views as PageViews
 from campaign import views as CampaignViews
@@ -28,6 +28,8 @@ urlpatterns = [
     url(r'^(?P<page_slug>[\w-]+)/campaign/(?P<campaign_slug>[\w-]+)/$', CampaignViews.campaign, name='campaign'),
 
     url(r'^$', views.home, name='home'),
-] 
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
 
