34ec0df9b25824067fcb8c4b3010d3b9160683dc
garrett <gn9012@gmail.com>
2017-08-30 18:41:44 -0500

    #54 -- added userprofile to user in admin

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from . import models
+
+
+class UserProfileInline(admin.StackedInline):
+    model = models.UserProfile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
