a71aec43e221b5b8e11a96d6d8c66f24bc8dda3a
garrett <gn9012@gmail.com>
2017-09-05 14:14:13 -0500

    #11 -- display replies on pages and campaigns via templatetags

---
+++ comments/templatetags/comment_extras.py
@@ -0,0 +1,13 @@
+from django import template
+from django.shortcuts import get_object_or_404
+
+from comments import models
+
+
+register = template.Library()
+
+@register.simple_tag
+def comment_replies(comment_pk):
+    comment = get_object_or_404(models.Comment, pk=comment_pk)
+    replies = models.Reply.objects.filter(comment=comment).order_by('-date')
+    return replies
