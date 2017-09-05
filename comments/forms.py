b508e72c8bed9ea7e7d8027cd7da3004e5f169fe
garrett <gn9012@gmail.com>
2017-09-05 16:43:40 -0500

    #11 -- basic jquery setup before building the ajax submission

--- comments/forms.py
+++ comments/forms.py
@@ -9,3 +9,12 @@ class CommentForm(forms.ModelForm):
         fields = [
             'content',
         ]
+        widgets = {
+            'content': forms.Textarea(
+                attrs={
+                    'id': 'comment-text',
+                    'required': True,
+                    'placeholder': 'Comment here'
+                }
+            )
+        }
