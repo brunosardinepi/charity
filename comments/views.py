4aab9d30adf495657108d2c57f00f9b784bde884
garrett <gn9012@gmail.com>
2017-09-11 20:25:23 -0500

    #72 -- added comments to campaign

--- comments/views.py
+++ comments/views.py
@@ -61,17 +61,25 @@ def reply(request, comment_pk):
 @login_required
 def comment_campaign(request, campaign_pk):
     campaign = get_object_or_404(Campaign, pk=campaign_pk)
-    form = forms.CommentForm()
     if request.method == 'POST':
-        form = forms.CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.user = request.user
-            comment.campaign = campaign
-            comment.save()
-            return HttpResponseRedirect(campaign.get_absolute_url())
-        else:
-            return Http404
+        comment_text = request.POST.get('comment_text')
+        response_data = {}
+
+        comment = models.Comment.objects.create(
+            user=request.user,
+            content=comment_text,
+            campaign=campaign
+        )
+
+        response_data['content'] = comment.content
+        response_data['user'] = "%s %s" % (comment.user.userprofile.first_name, comment.user.userprofile.last_name)
+        response_data['date'] = localtime(comment.date)
+        response_data['date'] = response_data['date'].strftime('%m/%d/%y %-I:%M %p')
+        response_data['id'] = comment.pk
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
 
 @login_required
 def delete(request, model, pk):
