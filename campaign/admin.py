95f1d7793a31c3086d22a7722be37fd2b007e227
garrett <gn9012@gmail.com>
2017-09-19 21:43:06 -0500

    #50 -- updated admin

--- campaign/admin.py
+++ campaign/admin.py
@@ -5,9 +5,9 @@ from . import models
 
 
 class CampaignAdmin(GuardedModelAdmin):
-    list_display = ('id', 'name', 'campaign_slug', 'page',)
+    list_display = ('id', 'name', 'campaign_slug', 'page', 'deleted',)
     ordering = ('id', 'name', 'campaign_slug', 'page',)
-    list_filter = ('page',)
+    list_filter = ('page', 'deleted',)
     filter_horizontal = ('campaign_admins', 'campaign_managers',)
 
 admin.site.register(models.Campaign, CampaignAdmin)
