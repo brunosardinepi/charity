62e07ca8ba1c009d2b76f2dcb07940213c9764e3
garrett <gn9012@gmail.com>
2017-09-22 16:23:04 -0500

    #82 -- new model for regular invitations

--- invitations/models.py
+++ invitations/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from campaign.models import Campaign
 from page.models import Page
@@ -16,7 +17,7 @@ class ManagerInvitation(models.Model):
     accepted = models.BooleanField(default=False)
     declined = models.BooleanField(default=False)
     key = models.CharField(max_length=32, default=random_key)
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(default=timezone.now)
     invite_to = models.EmailField()
     invite_from = models.ForeignKey(User, on_delete=models.CASCADE)
     page = models.ForeignKey(Page, null=True, blank=True, on_delete=models.CASCADE)
@@ -25,3 +26,12 @@ class ManagerInvitation(models.Model):
     manager_delete = models.BooleanField(default=False)
     manager_invite = models.BooleanField(default=False)
     manager_upload = models.BooleanField(default=False)
+
+class RegularInvitation(models.Model):
+    expired = models.BooleanField(default=False)
+    accepted = models.BooleanField(default=False)
+    declined = models.BooleanField(default=False)
+    key = models.CharField(max_length=32, default=random_key)
+    date_created = models.DateTimeField(default=timezone.now)
+    invite_to = models.EmailField()
+    invite_from = models.ForeignKey(User, on_delete=models.CASCADE)
