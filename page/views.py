6c2c19a74996420ee4d0cb6e2b623120eb3ef7f8
garrett <gn9012@gmail.com>
2017-10-05 19:10:43 -0500

    #22 -- check if customer card already exists when they want to save it as a new card

--- page/views.py
+++ page/views.py
@@ -418,12 +418,31 @@ def page_donate(request, page_pk):
             if form.cleaned_data['save_card'] == True:
                 if request.user.is_authenticated:
                     customer = stripe.Customer.retrieve("%s" % request.user.userprofile.stripe_customer_id)
-                    customer.sources.create(source=request.POST.get('stripeToken'))
+
+                    customer_cards_dict = stripe.Customer.retrieve(customer.id).sources.all(object='card')
+                    print("customer_cards_dict = %s" % customer_cards_dict)
+                    customer_cards_cleaned = {}
+                    for c in customer_cards_dict['data']:
+                        customer_cards_cleaned["%s" % c['fingerprint']] = c['id']
+                    print("customer_cards_cleaned = %s" % customer_cards_cleaned)
+                    card_check = stripe.Token.retrieve(request.POST.get('stripeToken'))
+                    print("card_check fingerprint = %s" % card_check['card']['fingerprint'])
+
+                    if card_check['card']['fingerprint'] in customer_cards_cleaned:
+                        print("tell the user this card already exists")
+#                        card_source = card_check['card']['id']
+                        card_source = customer_cards_cleaned[card_check['card']['fingerprint']]
+                        print("existing card_source = %s" % card_source)
+                    else:
+                        card_source = customer.sources.create(source=request.POST.get('stripeToken'))
+                        card_source = card_source.id
+                        print("card_source = %s" % card_source)
 
                     charge = stripe.Charge.create(
                         amount=amount,
                         currency="usd",
                         customer=customer.id,
+                        source=card_source,
                         description="$%s donation to %s." % (form.cleaned_data['amount'], page.name),
                         receipt_email=request.user.email,
                         destination={
