d379bcab24bc66a851a529d13458ba57f77891ed
garrett <gn9012@gmail.com>
2017-11-21 09:02:42 -0600

    #178 -- script to check end date and set ended campaigns to inactive

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ crontab -e
 0 2 * * * /bin/bash /home/[user]/pagefund/scripts/expire.sh
 0 * * * * /home/[user]/pagefund/pagefundenv/bin/python3 /home/[user]/pagefund/pagefund/trending_pages.py
 0 * * * * /home/[user]/pagefund/pagefundenv/bin/python3 /home/[user]/pagefund/pagefund/trending_campaigns.py
+* * * * * /home/[user]/pagefund/pagefundenv/bin/python3 /home/[user]/pagefund/manage.py campaign_end
 ```
 
 &copy; 2017 PageFund, Inc.
---
+++ campaign/management/commands/campaign_end.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+
+from campaign.models import Campaign
+
+
+class Command(BaseCommand):
+    help = 'Closes the campaigns that are set to end'
+
+    def handle(self, *args, **options):
+        campaigns = Campaign.objects.filter(end_date__lte=timezone.now(), is_active=True)
+        for c in campaigns:
+            c.is_active = False
+            c.save()
