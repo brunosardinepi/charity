2e70a59d61cc1d18d2bbec8baccc72569ed6e6d6
garrett <gn9012@gmail.com>
2017-12-22 11:16:54 -0600

    #210 -- using scss

--- pagefund/settings.py
+++ pagefund/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
+    'sass_processor',
     'userprofile',
     'page',
     'campaign',
