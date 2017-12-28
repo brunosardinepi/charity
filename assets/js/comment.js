9f7159d1480c2a27ad59961be4e2eeb02c46b7e8
garrett <gn9012@gmail.com>
2017-12-28 09:16:32 -0600

    #214 -- styling for comments

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -8,11 +8,11 @@ function comment() {
         },
         success : function(json) {
             $("#comment-text").val('');
-            if (!$("#comment_list").length) {
+            if (!$("#comment-list").length) {
                 $("#no-comments").remove();
-                $("#comment").after("<div id='comment_list'></div>");
+                $("#comment").after("<div id='comment-list'></div>");
             }
-            $("#comment_list").prepend("<div class='row comment' id='comment-" + json.id + "'><div class='col'><img src='" + json.user_image_url + "' /><br />" + json.user + "</div><div class='col'>" + json.content + "<hr>" + json.date + "<button id='upvotes-c-" + json.id + "' class='vote upvote user-vote'>1</button> <button id='downvotes-c-" + json.id + "' class='vote downvote'>0</button> - <a class='show-reply' id='" + json.id + "' href=''>Reply</a> - <a id='delete-comment-" + json.id + "' class='delete-cr' href=''>Delete</a>");
+            $("#comment-list").prepend("<div class='row comment' id='comment-" + json.id + "'><div class='col-md-auto'><img class='comment-profile-picture' src='" + json.user_image_url + "' /></div><div class='col'><div class='row'><div class='col'><span class='comment-user-name'>" + json.user + "</span><span class='comment-date'><i class='fal fa-clock'></i> " + json.date + "</span></div><div class='col text-right'><button id='upvotes-c-" + json.id + "' class='vote upvote user-vote'>1</button> <button id='downvotes-c-" + json.id + "' class='vote downvote'>0</button> - <a class='show-reply' id='" + json.id + "' href=''>Reply</a> - <a id='delete-comment-" + json.id + "' class='delete-cr' href=''>Delete</a></div></div><hr>" + json.content + "</div>");
         }
     });
 };
@@ -24,7 +24,6 @@ $(document).on("submit", "#comment", function(event) {
 
 function show_reply_textarea(event) {
     var url = "/comments/" + event.target.id + "/reply/";
-//    var url = "{% url 'comments:reply' comment_pk=0 %}".replace(0, event.target.id);
     $(event.target).parent().after("<form id='reply' action=" + url + " method='POST'><input type='hidden' name='csrfmiddlewaretoken' value='" + csrftoken + "' /><textarea id='reply-text' autofocus></textarea><input type='submit' value='Reply' /></form>");
 };
 
@@ -38,7 +37,7 @@ function reply(id) {
             if (!$("#comment-" + id + "-replies").length) {
                 $("#comment-" + id).after("<div id='comment-" + id + "-replies' class='reply-list'></div>");
             }
-            $("#comment-" + id + "-replies").append("<div class='row reply' id='reply-" + json.id + "'><div class='col'><img src='" + json.user_image_url + "' /><br />REPLY: " + json.user + "</div><div class='col'>" + json.content + "<hr>" + json.date + "<button id='upvotes-r-" + json.id + "' class='vote upvote user-vote'>1</button> <button id='downvotes-r-" + json.id + "' class='vote downvote'>0</button> - <a id='delete-reply-" + json.id + "' class='delete-cr' href=''>Delete</a>");
+            $("#comment-" + id + "-replies").append("<div class='row reply' id='reply-" + json.id + "'><div class='col-md-auto'><img class='comment-profile-picture' src='" + json.user_image_url + "' /></div><div class='col'><div class='row'><div class='col'><span class='comment-user-name'>" + json.user + "</span><span class='comment-date'><i class='fal fa-clock'></i> " + json.date + "</span></div><div class='col text-right'><button id='upvotes-r-" + json.id + "' class='vote upvote user-vote'>1</button> <button id='downvotes-r-" + json.id + "' class='vote downvote'>0</button> - <a id='delete-reply-" + json.id + "' class='delete-cr' href=''>Delete</a></div></div><hr>" + json.content + "</div>");
         }
     });
 };
@@ -57,7 +56,6 @@ $(document).on('submit', "#reply", function(event) {
 function delete_obj(event) {
     var arr = event.target.id.split('-');
     var url = "/comments/delete/" + arr[1] + "/" + arr[2] + "/";
-//    var url = "{% url 'comments:delete' model=0 pk=1 %}".replace(0, arr[1]).replace(1, arr[2]);
     $.get(url, function () {
         $("#" + arr[1] + "-" + arr[2]).remove();
     });
