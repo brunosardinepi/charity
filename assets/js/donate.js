c490fb65d65065fdb5a5294f8f2e74dd35e813f8
garrett <gn9012@gmail.com>
2018-01-03 20:35:44 -0600

    #214 -- donate via modal working

--- assets/js/donate.js
+++ assets/js/donate.js
@@ -36,34 +36,34 @@ $(document).on('click', "[id^='show-donate']", function(event) {
     var url = "/profile/card/list/";
 
     $.get(url, function (data) {
+        var cards = "";
         $.each(data, function(key, value) {
             console.log("default = " + value.default);
             var checked = ((value.default == true) ? 'checked' : '');
             console.log("checked = " + checked);
             cards += "<input type='radio' name='saved_card' class='saved-card' value='" + value.id + "' " + checked + "/> " + value.name + " (" + value.last4 + ")<br />";
-            console.log("current cards after latest addition = " + cards);
         });
+        console.log("cards = " + cards);
+        var url = "/page/" + pk + "/donate/";
+
+       $('#donateModal .modal-body').append("<form action=" + url + " method='POST' id='payment-form'><input type='hidden' name='csrfmiddlewaretoken' value='" + csrftoken + "' /><input type='checkbox' name='amount' class='preset-amount' value=5 />$5<br /><input type='checkbox' name='amount' class='preset-amount' value=10 />$10<br /><input type='checkbox' name='amount' class='preset-amount' value=25 />$25<br /><input type='checkbox' name='amount' class='preset-amount' value=50 />$50<br /><input type='checkbox' name='amount' class='preset-amount' value=100 />$100<br /><div class='input-group'><div class='input-group-prepend'><span class='input-group-text'>$</span></div><input type='number' class='form-control' id='id_amount' min='0' max='999999' aria-label='Amount' placeholder='Custom amount'></div><div class='form-check'><input type='checkbox' name='anonymous_amount' class='form-check-input' id='id_anonymous_amount'><label class='form-check-label' for='id_anonymous_amount'>Anonymous amount</label></div><div id='amount-errors'></div><div class='form-check'><input type='checkbox' name='anonymous_donor' class='form-check-input' id='id_anonymous_donor'><label class='form-check-label' for='id_anonymous_donor'>Anonymous donor</label></div><div class='form-group'><textarea class='form-control' name='comment' id='id_comment' rows='3' placeholder='Type your comment here (optional)'></textarea></div><div class='form-check'><input type='checkbox' name='monthly' class='form-check-input' id='id_monthly'><label class='form-check-label' for='id_monthly'>Monthly</label></div>" + cards + "<p><a id='new-card' href=''>New card</a></p><div id='new-card-info' style='display:none;'><p><label for='id_save_card'>Save card:</label> <input type='checkbox' name='save_card' id='id_save_card' /></p><div id='card-element'></div><div id='card-errors' role='alert'></div></div></form>");
+
+        var stripe = Stripe('{{ api_pk }}');
+        var elements = stripe.elements();
+        var card = elements.create('card');
+        card.mount("#card-element");
+
+        card.addEventListener('change', function(event) {
+            var displayError = document.getElementById('card-errors');
+            if (event.error) {
+                displayError.textContent = event.error.message;
+            } else {
+                displayError.textContent = '';
+            }
+        });
+
+        $('#donateModal .modal-footer').html("<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button><button type='button' class='btn btn-primary submit-donate' data-dismiss='modal'>Donate</button>");
     });
-
-    var url = "/page/" + pk + "/donate/";
-
-    $('#donateModal .modal-body').append("<form action=" + url + " method='POST' id='payment-form'><input type='hidden' name='csrfmiddlewaretoken' value='" + csrftoken + "' /><input type='checkbox' name='amount' class='preset-amount' value=5 />$5<br /><input type='checkbox' name='amount' class='preset-amount' value=10 />$10<br /><input type='checkbox' name='amount' class='preset-amount' value=25 />$25<br /><input type='checkbox' name='amount' class='preset-amount' value=50 />$50<br /><input type='checkbox' name='amount' class='preset-amount' value=100 />$100<br /><div class='input-group'><div class='input-group-prepend'><span class='input-group-text'>$</span></div><input type='number' class='form-control' id='id_amount' min='0' max='999999' aria-label='Amount' placeholder='Custom amount'></div><div class='form-check'><input type='checkbox' name='anonymous_amount' class='form-check-input' id='id_anonymous_amount'><label class='form-check-label' for='id_anonymous_amount'>Anonymous amount</label></div><div id='amount-errors'></div><div class='form-check'><input type='checkbox' name='anonymous_donor' class='form-check-input' id='id_anonymous_donor'><label class='form-check-label' for='id_anonymous_donor'>Anonymous donor</label></div><div class='form-group'><textarea class='form-control' name='comment' id='id_comment' rows='3' placeholder='Type your comment here (optional)'></textarea></div><div class='form-check'><input type='checkbox' name='monthly' class='form-check-input' id='id_monthly'><label class='form-check-label' for='id_monthly'>Monthly</label></div><p><a id='new-card' href=''>New card</a></p><div id='new-card-info' style='display:none;'><p><label for='id_save_card'>Save card:</label> <input type='checkbox' name='save_card' id='id_save_card' /></p><div id='card-element'></div><div id='card-errors' role='alert'></div></div>");
-
-    var stripe = Stripe('{{ api_pk }}');
-    var elements = stripe.elements();
-    var card = elements.create('card');
-    card.mount("#card-element");
-
-    card.addEventListener('change', function(event) {
-        var displayError = document.getElementById('card-errors');
-        if (event.error) {
-            displayError.textContent = event.error.message;
-        } else {
-            displayError.textContent = '';
-        }
-    });
-
-    $('#donateModal .modal-footer').html("<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button><button type='button' class='btn btn-primary submit-donate' data-dismiss='modal'>Donate</button>");
 });
 
 $(document).on("click", ".submit-donate", function(event) {
