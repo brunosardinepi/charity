17384713395ad3544a406eea659dd60dacd7a401
garrett <gn9012@gmail.com>
2018-01-19 15:39:56 -0600

    #259 -- styling for my pages and campaigns

--- userprofile/templatetags/userprofile_extras.py
+++ userprofile/templatetags/userprofile_extras.py
@@ -7,17 +7,17 @@ register = template.Library()
 def user_admin_status(user, type, obj):
     if type == "page":
         if user.userprofile in obj.admins.all():
-            return "owner"
+            return "Owner"
         elif user.userprofile in obj.managers.all():
-            return "manager"
+            return "Manager"
         else:
-            return ""
+            return "Subscribed"
     elif type == "campaign":
         if user.userprofile in obj.campaign_admins.all():
-            return "owner"
+            return "Owner"
         elif user.userprofile in obj.campaign_managers.all():
-            return "manager"
+            return "Manager"
         else:
-            return ""
+            return "Subscribed"
     else:
         return ""
