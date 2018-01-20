689eff1b73236aa52d353aabd85f59040214cdae
garrett <gn9012@gmail.com>
2018-01-19 19:45:59 -0600

    #259 -- redo upload

--- assets/js/image_upload.js
+++ assets/js/image_upload.js
@@ -7,11 +7,10 @@ $(function () {
         dataType: 'json',
         done: function (event, data) {
             if (data.result.is_valid == 't') {
-                if (!$("#image-list").length) {
-                    $("<ul id='image-list'></ul>").insertAfter("#images-header");
-                    $("p:contains('No images!')").remove();
+                if(!$(".col-4").length) {
+                    $("#no-images").remove();
                 };
-                $("#image-list").prepend("<li id='image-" + data.result.pk + "'><span class='profile' id='profile-" + data.result.pk + "'><a class='set-profile-picture' href=''>Make profile picture</a> - </span><a href='" + data.result.url + "'>" + data.result.name + "</a> - <a id='delete-" + data.result.pk + "' class='delete-image' href=''>Delete</a></li>");
+                $("#image-list").prepend("<div class='col-4' id='image-" + data.result.pk + "'><div class='card' style='border:0;'><img class='card-img-top' src='" + data.result.url + "'><div class='card-body p-0 pt-1'><div class='row justify-content-between'><div class='col'><span class='small profile' id='profile-" + data.result.pk + "'><a class='set-profile-picture' href=''>Make profile picture</a></span></div><div class='col-3 text-right'><a id='delete-" + data.result.pk + "' class='delete-image' href=''><i class='fal fa-trash-alt'></i></a></div></div></div></div></div>");
             } else {
                 if (data.result.redirect == "error_size") {
                     var url = "/error/image/size/";
