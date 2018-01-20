a19c94d2dc6c4598331c7fa8ed447548fb07d443
garrett <gn9012@gmail.com>
2018-01-19 22:16:05 -0600

    fixes #309

--- assets/js/image_upload.js
+++ assets/js/image_upload.js
@@ -13,9 +13,9 @@ $(function () {
                 $("#image-list").prepend("<div class='col-4' id='image-" + data.result.pk + "'><div class='card' style='border:0;'><img class='card-img-top' src='" + data.result.url + "'><div class='card-body p-0 pt-1'><div class='row justify-content-between'><div class='col'><span class='small profile' id='profile-" + data.result.pk + "'><a class='set-profile-picture' href=''>Make profile picture</a></span></div><div class='col-3 text-right'><a id='delete-" + data.result.pk + "' class='delete-image' href=''><i class='fal fa-trash-alt'></i></a></div></div></div></div></div>");
             } else {
                 if (data.result.redirect == "error_size") {
-                    var url = "/error/image/size/";
+                    var url = "/notes/error/image/size/";
                 } else if (data.result.redirect == "error_type") {
-                    var url = "/error/image/type/";
+                    var url = "/notes/error/image/type/";
                 };
                 window.location.href = url;
             };
