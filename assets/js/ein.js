41a645a59444d26e73c79188719f00eae92b2145
garrett <gn9012@gmail.com>
2018-01-28 19:30:32 -0600

    #288 -- js re-organization for page

--- assets/js/ein.js
+++ assets/js/ein.js
@@ -5,3 +5,19 @@ $(document).on("change", "#id_type", function() {
         $("#ein-row").hide()
     }
 });
+
+$(document).on("click", "#form-input", function(event) {
+    event.preventDefault();
+    var type = $("#id_type");
+    var ein = $("#id_ein");
+    if (type.val() == "nonprofit") {
+        if (ein.val().length) {
+            $("#create-form").submit();
+        } else {
+            alert("Please add your EIN.");
+        };
+    } else {
+        $("#create-form").submit();
+    };
+});
+
---
+++ assets/js/sort_page_donations.js
@@ -0,0 +1,50 @@
+function sort(event, column) {
+    var parent_th = $(event.target).parent("th");
+    if (parent_th.hasClass("desc")) {
+        var sort_by = "asc";
+    } else {
+        var sort_by = "desc";
+    };
+
+    var url = "/page/dashboard/ajax/donations/";
+
+    $.ajax({
+        url : url,
+        type : "POST",
+        traditional: true,
+        data : {
+            page_pk : page_pk,
+            sort_by : sort_by,
+            column : column,
+        },
+        success : function(result) {
+            $("#donation-history-body").empty();
+            parent_th.attr("class", sort_by);
+            for (var key in result) {
+                var date = result[key]["date"];
+                if (result[key]["anonymous_amount"] == true) {
+                    var amount = "Anonymous";
+                } else if (result[key]["anonymous_amount"] == false) {
+                    var amount = "$" + result[key]['amount'] / 100;
+                };
+                if (result[key]["campaign"]) {
+                    var campaign = result[key]["campaign"];
+                } else {
+                    var campaign = "--";
+                };
+                if (result[key]["anonymous_donor"] == true) {
+                    var donor = "Anonymous";
+                } else if (result[key]["anonymous_donor"] == false) {
+                    var donor = result[key]["user"]["first_name"] + " " + result[key]["user"]["last_name"];
+                };
+                $("#donation-history-body").append("<tr><td>" + date + "</td><td>" + amount + "</td><td>" + campaign + "</td><td>" + donor + "</td></tr>");
+            }
+        }
+    });
+};
+
+$(document).on("click", ".sort", function(event) {
+    event.preventDefault();
+    var column = $(event.target).attr("id");
+    sort(event, column);
+});
