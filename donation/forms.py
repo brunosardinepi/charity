f1dadfa27de7a7fac7f8518ea0d9b29c9bf9179a
garrett <gn9012@gmail.com>
2018-01-29 14:47:00 -0600

    #343 -- styling for donate, missing button group style on mobile

--- donation/forms.py
+++ donation/forms.py
@@ -4,7 +4,10 @@ from django import forms
 class BaseDonate(forms.Form):
     anonymous_amount = forms.BooleanField(required=False)
     anonymous_donor = forms.BooleanField(required=False)
-    comment = forms.CharField(widget=forms.Textarea, required=False)
+    comment = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': 'Type your comment here (optional)',
+        'rows': 3,
+        }), required=False)
     amount = forms.IntegerField(min_value=0, max_value=999999, required=False)
 
 class DonateForm(BaseDonate):
