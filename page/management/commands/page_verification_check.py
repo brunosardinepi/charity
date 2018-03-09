ce3d543b553d359055d6ffb6f27eec605af7f202
garrett <gn9012@gmail.com>
2018-03-09 15:14:55 +0000

    updated stripe verification check to allow pending pages

--- page/management/commands/page_verification_check.py
+++ page/management/commands/page_verification_check.py
@@ -12,9 +12,8 @@ class Command(BaseCommand):
         pages = Page.objects.all()
         for page in pages:
             account = stripe.Account.retrieve(page.stripe_account_id)
-            print(account['legal_entity']['verification']['status'])
             status = account['legal_entity']['verification']['status']
-            if status == 'verified':
+            if status == 'verified' or status == 'pending':
                 page.stripe_verified = True
             else:
                 page.stripe_verified = False
